5c7ef5c29ac855ec43318ebf3b8693d257ee9d1a
VishnuReddy3011 <sajjalavishnuvardhanreddy2001@gmail.com>
2024-07-04 22:05:44 +0530

    changed in comment

--- JavaScript/class 4/FlattenAnArray.js
+++ JavaScript/class 4/FlattenAnArray.js
@@ -29,7 +29,7 @@ const arr = [1, 2, 3, [4, 5, 6], [7, 8], 9];
 // console.log(arr);
 // console.log(res);
 
-// Implementing myFlat to the prototype, same logic
+// Implementing myFlat polyfill, same logic
 Array.prototype.myFlat2 = function () {
   const arr = this;
   if(!arr || !Array.isArray(arr)) {
